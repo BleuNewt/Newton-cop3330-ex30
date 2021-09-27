361a0195abf2090817f32c76e93e9796e0caec99
Ryan Newton <Rjnewton0620@gmail.com>
2021-09-27 18:54:03 -0400

    Initial commit

---
+++ src/main/java/org/example/Exercise30.java
@@ -0,0 +1,17 @@
+package org.example;
+/*  UCF COP3330 Fall 2021 Assignment 2 Solution
+*  Copyright 2021 Ryan Newton*/
+public class Exercise30
+{
+    public static void main(String[] args)
+    {
+        for(int a=1;a<=12;a++) //for loop 1
+        {
+            for(int b=1;b<=12;b++)//for loop 2
+            {
+                System.out.print((a*b)+"\t"); //print
+            }
+            System.out.println(); //new line
+        }
+    }
+}
